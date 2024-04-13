5f7e39c811ff9cee5da7c90d62b4d6341f84dce2
samsung-ga <maxwodyd98@gmail.com>
2024-04-13 14:39:19 +0900

    :tada: Init project

---
+++ MoMo/MoMo/Source/Network/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  MoMo
+//
+//  Created by Woody Lee on 4/13/24.
+//
+
+import Foundation
---
+++ MoMo/MoMo/Source/Scene/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  MoMo
+//
+//  Created by Woody Lee on 4/13/24.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+}
